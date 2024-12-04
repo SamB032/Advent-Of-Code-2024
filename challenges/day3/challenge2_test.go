224f8445f1c99d983a2a03658e91464d74fb8688
Sam <sam.boffey04@gmail.com>
2024-12-04 18:36:14 +0000

    CI fixes

--- challenges/day3/challenge2_test.go
+++ challenges/day3/challenge2_test.go
@@ -7,11 +7,11 @@ import (
 	util "github.com/samb032/advent-of-code-2024/utils"
 )
 
-func TestDay3challenge2(t *testing.t) {
+func TestDay3challenge2(t *testing.T) {
 	input := util.readfile("input.txt")
-	inputjoined := strings.join(input, "")
+	inputJoined := strings.Join(input, "")
 
-	got := Day3challenge2(inputjoined)
+	got := Day3Challenge2(inputJoined)
 
 	if got != 48 {
 		t.errorf("Day3challenge2 is incorrect. got: %d, want: %d", got, 48)
