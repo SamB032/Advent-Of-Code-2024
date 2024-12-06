5016b0a43f568c181a16f3f5cc19c3338ce7c89f
Sam <sam.boffey04@gmail.com>
2024-12-06 13:10:09 +0000

    Day1 challenge

---
+++ utils/breathFirstSearch.go
@@ -0,0 +1,23 @@
+package util
+
+type Queue struct {
+  Arr  []int 
+  Size int
+}
+
+//Adds the item to the top of the array
+func (queue *Queue) Push(element int) {
+  queue.Arr = append(queue.Arr, element)
+  queue.Size++
+}
+
+//Removes the item in the first
+func (queue *Queue) Pop() int {
+  if queue.Size <= 0 {
+    return 0
+  }
+
+  element := queue.Arr[0] // Get the first element
+  queue.Arr = RemoveIndex(queue.Arr, 0)
+  return element
+}
