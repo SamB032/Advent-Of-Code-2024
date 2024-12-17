606fcd9fe5171eb1c10dd22cb93dc62ad6807a21
Sam <sam.boffey04@gmail.com>
2024-12-17 17:03:39 +0000

    Add stack

---
+++ utils/depthFirstSearch.go
@@ -0,0 +1,23 @@
+package util
+
+type Stack[T any] struct {
+	Arr  []*T
+	Size int
+}
+
+// Adds the item to the top of the array
+func (stack *Stack[T]) Push(element *T) {
+	stack.Arr = append(stack.Arr, element)
+	stack.Size++
+}
+
+// Removes the item in the first
+func (stack *Stack[T]) Pop() *T {
+	if stack.Size <= 0 {
+		panic("stack Empty")
+	}
+	element := stack.Arr[stack.Size - 1] // Get the last element
+	stack.Arr = RemoveIndex(stack.Arr, stack.Size - 1)
+	stack.Size--
+	return element
+}
